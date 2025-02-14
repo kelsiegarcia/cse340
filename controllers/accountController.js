3cdb3abfe8164621eee3b8b51161069c816f61ab
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2025-02-13 21:37:48 -1000

    week 06 from validations client and server side

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,6 @@
 const utilities = require('../utilities/');
+const accountModel = require('../models/account-model');
+const bcrypt = require('bcryptjs');
 
 /* ****************************************
  *  Deliver login view
@@ -8,6 +10,7 @@ async function buildLogin(req, res, next) {
   res.render('account/login', {
     title: 'Login',
     nav,
+    errors: null,
   });
 }
 
@@ -19,7 +22,87 @@ async function buildRegister(req, res, next) {
   res.render('account/register', {
     title: 'Register',
     nav,
+    errors: null,
   });
 }
 
-module.exports = { buildLogin, buildRegister };
+/* ****************************************
+ *  Process Registration
+ * *************************************** */
+async function registerAccount(req, res) {
+  let nav = await utilities.getNav();
+  const {
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password,
+  } = req.body;
+
+  // Hash the password before storing
+  let hashedPassword;
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10);
+  } catch (error) {
+    req.flash(
+      'notice',
+      'Sorry, there was an error processing the registration.'
+    );
+    res.status(500).render('account/register', {
+      title: 'Registration',
+      nav,
+      errors: null,
+    });
+  }
+
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    hashedPassword
+  );
+
+  if (regResult) {
+    req.flash(
+      'notice',
+      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+    );
+    res.status(201).render('account/login', {
+      title: 'Login',
+      nav,
+    });
+  } else {
+    req.flash('notice', 'Sorry, the registration failed.');
+    res.status(501).render('account/register', {
+      title: 'Registration',
+      nav,
+    });
+  }
+}
+
+/* ****************************************
+ *  process login request
+ * *************************************** */
+
+async function processLogin(req, res) {
+  let nav = await utilities.getNav();
+  const { account_email, account_password } = req.body;
+  const loginResult = await accountModel.loginAccount(account_email);
+
+  if (loginResult) {
+    req.flash('notice', 'Login successful');
+    res.status(200).render('account/login', {
+      title: 'Login',
+      nav,
+    });
+  } else {
+    req.flash('notice', 'Sorry, the login failed.');
+    res.status(400).render('account/login', {
+      title: 'login',
+      nav,
+      errors: null,
+    });
+  }
+}
+
+module.exports = { buildLogin, buildRegister, registerAccount, processLogin };
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const utilities = require('../utilities/');
 const accountController = require('../controllers/accountController');
+const regValidate = require('../utilities/account-validation');
 
 router.get('/login', utilities.handleErrors(accountController.buildLogin));
 
@@ -9,5 +10,25 @@ router.get(
   '/register',
   utilities.handleErrors(accountController.buildRegister)
 );
+
+// POST request for registering a new account
+router.post(
+  '/register',
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
+
+// Process the login attempt
+// router.post(
+//   '/login',
+//   regValidate.loginRules(),
+//   regValidate.checkLoginData,
+//   utilities.handleErrors(accountController.processLogin)
+// );
+
+router.post('/login', (req, res) => {
+  res.status(200).send('login process');
+});
 
 module.exports = router;
